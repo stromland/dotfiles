e5490da2857c17e786345894326b46017810d4e6
Espen Strømland <espen.stromland@gmail.com>
2023-04-15 11:27:49 +0200

    chore(nvim): theme

--- nvim/nvchad/custom/plugins.lua
+++ nvim/nvchad/custom/plugins.lua
@@ -1,14 +1,12 @@
 local plugins = {
   {
   "neovim/nvim-lspconfig",
-
    dependencies = {
      "jose-elias-alvarez/null-ls.nvim",
      config = function()
        require "custom.configs.null-ls"
      end,
    },
-
    config = function()
       require "plugins.configs.lspconfig"
       require "custom.configs.lspconfig"
@@ -22,6 +20,30 @@ local plugins = {
         "golangci-lint",
       },
     },
+  },
+  {
+    "NvChad/nvim-colorizer.lua",
+    opts = {
+      user_default_options = {
+        names = false
+      }
+    }
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "lua",
+        "make",
+        "go",
+        "json",
+        "yaml",
+        "html",
+        "javascript",
+        "typescript",
+        "markdown",
+      },
+    }
   }
 }
 
